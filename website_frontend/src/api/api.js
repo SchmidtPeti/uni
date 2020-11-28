561067b5d6fe6a64eaae43afab2b053a5751f180
schmidt peti <schmidtpeti@schmidts-MacBook-Air.local>
2020-11-28 13:28:25 +0100

    backend change

--- website_frontend/src/api/api.js
+++ website_frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: process.env.REACT_API_URL,
 })
 
 export const insertMatAlapTask = payload => api.post(`/matalap_task`, payload)
